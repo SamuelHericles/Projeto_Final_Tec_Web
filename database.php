e50bb302e768cf901c18ad757e19c478c52ecd5c
GeronimoAguiar <geron@alu.ufc.br>
2019-11-12 12:32:15 -0300

    database modificado

---
+++ database.php
@@ -0,0 +1,122 @@
+<?php
+    class Cliente{
+        private $id;
+        private $nome;
+        public $email;
+        private $senha;
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function setId($id){
+            $this->id = $id;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function setNome($nome){
+            $this->nome = $nome;
+        }
+
+        public function getSenha(){
+            return $this->senha;
+        }
+
+        public function setSenha($senha){
+            $this->senha = $senha;
+        }
+
+        public function getAllCliente(){
+            $conexao = new PDO('mysql:host=127.0.0.1:3306;dbname=emp','root','1234');
+            $query = "SELECT * FROM cliente";
+            $lista_sql = $conexao->query($query);
+            $lista = $lista_sql->fetchAll();//converte para lista php
+            return $lista;
+        }
+
+        public function getVenda(){
+            $conexao = new PDO('mysql:host=127.0.0.1:3306;dbname=emp','root','1234');
+            $query = "SELECT * FROM venda WHERE id_cliente = $this->id";
+            $lista_sql = $conexao->query($query);
+            $lista = $lista_sql->fetchAll();//converte para lista php
+            return $lista;
+        }
+    }
+
+    class Funcionario{
+        private $id;
+        private $nome;
+        public $email;
+        private $senha;
+        public $cargo;
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function setId($id){
+            $this->id = $id;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function setNome($nome){
+            $this->nome = $nome;
+        }
+
+        public function getSenha(){
+            return $this->senha;
+        }
+
+        public function setSenha($senha){
+            $this->senha = $senha;
+        }
+
+        public function getAllFuncionario(){
+            $conexao = new PDO('mysql:host=127.0.0.1:3306;dbname=emp','root','1234');
+            $query = "SELECT * FROM funcionario";
+            $lista_sql = $conexao->query($query);
+            $lista = $lista_sql->fetchAll();//converte para lista php
+            return $lista;
+        }
+
+    }
+
+    class Produto{
+        public $id;
+        public $nome;
+        public $preço;
+        public $quantidade;
+        public $categoria;
+        public $descricao;
+
+        public function getAllProduto(){
+            $conexao = new PDO('mysql:host=127.0.0.1:3306;dbname=emp','root','1234');
+            $query = "SELECT * FROM produtos";
+            $lista_sql = $conexao->query($query);
+            $lista = $lista_sql->fetchAll();//converte para lista php
+            return $lista;
+        }
+    }
+
+    class Venda{
+        public $id;
+        public $id_cliente;
+        public $id_produto;
+        public $andamento;
+
+        public function getAllVenda(){
+            $conexao = new PDO('mysql:host=127.0.0.1:3306;dbname=emp','root','1234');
+            $query = "SELECT * FROM vanda";
+            $lista_sql = $conexao->query($query);
+            $lista = $lista_sql->fetchAll();//converte para lista php
+            return $lista;
+        }
+    }
+
+?>
