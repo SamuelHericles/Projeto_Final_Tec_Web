17ddffd75008d6d6a2ee05ccdc12d8af13792802
Daniel Jorge <danieljorge17@hotmail.com>
2019-11-20 23:15:02 -0300

    Inserido o link por email

--- Contato.php
+++ Contato.php
@@ -8,9 +8,9 @@
         <div class="container"> 
           <h1 class="h1page">Formulário para contato</h1>
 
-          <form id="contatoForm" class="formone" method="GET" action="mailto:sisteccorporation@outlook.com" target="_blank">
+          <form class="formone" method="POST" action="https://formspree.io/danieljorge17@hotmail.com">
 
-                  
+
                       <div class="form-group">
                       <label>Nome completo:</label>
                       <input type="text" class="form-control" name="nome" placeholder="Francisco João da Silva">
@@ -38,7 +38,7 @@
                       </div>
                   
                       <div class="form-group">
-                  <input id="btnEnviaContato" type="submit" name="BTEnvia" class="btn btn-primary btn-lg btn-block btn-dark" value="Enviar">
+                          <input id="btnEnviaContato" type="submit" class="btn btn-primary btn-lg btn-block btn-dark" value="Enviar">
                       </div>
           </form>
           
@@ -60,38 +60,7 @@
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="js/vendor/jquery-slim.min.js"><\/script>')</script>
       <script src="js/bootstrap.min.js"></script>
-      <script>document.getElementById('contatoForm').addEventListener('submit', function () {
-                var nome = this.querySelector('input[name=nome]'), nome = nome.value;
-                var email = this.querySelector('input[name=email]'), email = email.value;
-                var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
-                this.querySelector('input[name=mensagem]').setAttribute('value', texto);
-            });
-      </script>    
+ 
 
 </body>
-<!--
-const createTransport = require('nodemailer').createTransport;
-
-const transporter = createTransport({
-  host: process.env.SMTP_SERVER,
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: process.env.TRANSPORTER_EMAIL,
-    pass: process.env.TRANSPORTER_PASS,
-  },
-});
-
-async function email(options) {
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(options, (error, info) => {
-      if (error) {
-        reject(error);
-      }
-      resolve((info || { messageId: 0 }).messageId);
-    });
-  });
-}
-
-module.exports = email;-->
 </html>
